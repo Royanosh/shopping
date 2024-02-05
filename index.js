98cd27f94bec9455173ff2eadaad2b0656a788d1
umeshkumawat <umesh@swipepages.com>
2024-02-05 21:40:48 +0530

    shopping init

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    type:'success',
+    data:'Hello From shopping'
+  });
+})
+
+app.listen(6003, () => {
+  console.log("Shopping server is running on Port Number: 6003");
+})
